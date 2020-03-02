1330dcdd85d479fa54377d565d2cf04366f814c3
Willian <willian.agostini@gmail.com>
2020-03-02 18:42:06 -0300

    add mock do hardware

---
+++ mqtt-node/index.js
@@ -0,0 +1,20 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://localhost')
+ 
+client.on('connect', function () {
+      setInterval(() => {
+        client.publish('caveira', randomInt(0,25))
+        client.publish('pirata', randomInt(10,30))
+    }, 1000);
+    }
+)
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
+
+function randomInt(low, high) {
+  return (Math.floor(Math.random() * (high - low) + low)).toString()
+}
